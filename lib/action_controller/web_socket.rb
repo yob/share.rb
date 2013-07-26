ba8cbb1a508cdea2c3950029c2fa48b3309f3cbe
James Healy <james@yob.id.au>
2013-07-26 23:05:04 +1000

    remove action_controller integration

--- lib/action_controller/web_socket.rb
+++
@@ -1,8 +0,0 @@
-module ActionController
-  module WebSocket
-    def websocket_upgrade(socket_application)
-      upgrade_response = socket_application.call(request.env)
-      self.status, self.headers, self.response_body = upgrade_response
-    end
-  end
-end
