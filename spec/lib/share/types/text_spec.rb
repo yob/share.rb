1030c6ebdcaeecb33a58ba30d83cf19a1d616104
James Healy <james@yob.id.au>
2013-07-28 09:59:13 +1000

    convert Text specs to only call #transform
    * the transform_x method is an implementation detail that I'd like to
      make private

--- spec/lib/share/types/text_spec.rb
+++ spec/lib/share/types/text_spec.rb
@@ -13,27 +13,28 @@ describe Share::Types::Text do
     end
 
     it "inserts" do
-      text.transform_x([{'i' =>'x', 'p' =>9}], [{'i' =>'a', 'p' =>1}]).should == [[{'i' =>'x', 'p' =>10}], [{'i' =>'a', 'p' =>1}]]
-      text.transform_x([{'i' =>'x', 'p' =>10}], [{'i' =>'a', 'p' =>10}]).should == [[{'i' =>'x', 'p' =>10}], [{'i' =>'a', 'p' =>11}]]
+      text.transform([{'i' =>'x', 'p' =>9}], [{'i' =>'a', 'p' =>1}, {'i' =>'a', 'p' =>100}], "left").should == [{'i' =>'x', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>9},{'i' =>'y', 'p' =>10}], [{'i' =>'a', 'p' =>1}], "left").should == [{'i' =>'xy', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>9},{'i' =>'y', 'p' =>11}], [{'i' =>'a', 'p' =>1}], "left").should == [{'i' =>'x', 'p' =>10},{'i' =>'y', 'p' =>12}]
 
-      text.transform_x([{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>9}]).should == [[{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>9}]]
-      text.transform_x([{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>10}]).should == [[{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>10}]]
-      text.transform_x([{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>11}]).should == [[{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>12}]]
+      text.transform([{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>9},{'d' =>'a', 'p' =>100}], "left").should == [{'i' =>'x', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>10},{'d' =>'a', 'p' =>100}], "left").should == [{'i' =>'x', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>11}], [{'d' =>'a', 'p' =>11},{'d' =>'a', 'p' =>100}], "left").should == [{'i' =>'x', 'p' =>11}]
 
-      text.transform([{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>11}], 'left').should == [{'i' =>'x', 'p' =>10}]
-      text.transform([{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>10}], 'left').should == [{'i' =>'x', 'p' =>10}]
-      text.transform([{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>10}], 'right').should == [{'i' =>'x', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>11},{'d' =>'a', 'p' =>100}], 'left').should == [{'i' =>'x', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>10},{'d' =>'a', 'p' =>100}], 'left').should == [{'i' =>'x', 'p' =>10}]
+      text.transform([{'i' =>'x', 'p' =>10}], [{'d' =>'a', 'p' =>10},{'d' =>'a', 'p' =>100}], 'right').should == [{'i' =>'x', 'p' =>10}]
     end
 
     it "deletes" do
-      text.transform_x([{'d' =>'abc', 'p' =>10}], [{'d' =>'xy', 'p' =>4}]).should == [[{'d' =>'abc', 'p' =>8}], [{'d' =>'xy', 'p' =>4}]]
-      text.transform_x([{'d' =>'abc', 'p' =>10}], [{'d' =>'b', 'p' =>11}]).should == [[{'d' =>'ac', 'p' =>10}], []]
-      text.transform_x([{'d' =>'b', 'p' =>11}], [{'d' =>'abc', 'p' =>10}]).should == [[], [{'d' =>'ac', 'p' =>10}]]
-      text.transform_x([{'d' =>'abc', 'p' =>10}], [{'d' =>'bc', 'p' =>11}]).should == [[{'d' =>'a', 'p' =>10}], []]
-      text.transform_x([{'d' =>'abc', 'p' =>10}], [{'d' =>'ab', 'p' =>10}]).should == [[{'d' =>'c', 'p' =>10}], []]
-      text.transform_x([{'d' =>'abc', 'p' =>10}], [{'d' =>'bcd', 'p' =>11}]).should == [[{'d' =>'a', 'p' =>10}], [{'d' =>'d', 'p' =>10}]]
-      text.transform_x([{'d' =>'bcd', 'p' =>11}], [{'d' =>'abc', 'p' =>10}]).should == [[{'d' =>'d', 'p' =>10}], [{'d' =>'a', 'p' =>10}]]
-      text.transform_x([{'d' =>'abc', 'p' =>10}], [{'d' =>'xy', 'p' =>13}]).should == [[{'d' =>'abc', 'p' =>10}], [{'d' =>'xy', 'p' =>10}]]
+      text.transform([{'d' =>'abc', 'p' =>10}], [{'d' =>'xy', 'p' =>4},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'abc', 'p' =>8}]
+      text.transform([{'d' =>'abc', 'p' =>10}], [{'d' =>'b', 'p' =>11},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'ac', 'p' =>10}]
+      text.transform([{'d' =>'b', 'p' =>11}], [{'d' =>'abc', 'p' =>10},{'d' =>'a', 'p' =>100}], "left").should == []
+      text.transform([{'d' =>'abc', 'p' =>10}], [{'d' =>'bc', 'p' =>11},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'a', 'p' =>10}]
+      text.transform([{'d' =>'abc', 'p' =>10}], [{'d' =>'ab', 'p' =>10},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'c', 'p' =>10}]
+      text.transform([{'d' =>'abc', 'p' =>10}], [{'d' =>'bcd', 'p' =>11},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'a', 'p' =>10}]
+      text.transform([{'d' =>'bcd', 'p' =>11}], [{'d' =>'abc', 'p' =>10},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'d', 'p' =>10}]
+      text.transform([{'d' =>'abc', 'p' =>10}], [{'d' =>'xy', 'p' =>13},{'d' =>'a', 'p' =>100}], "left").should == [{'d' =>'abc', 'p' =>10}]
     end
   end
 
