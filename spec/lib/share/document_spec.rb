479128de9df0441371d1c7e78d8606a7f0b5f575
James Healy <james@yob.id.au>
2013-07-27 14:43:26 +1000

    break document spec into 2 files
    * 1 for unit-ish specs
    * 1 for integration specs

--- spec/lib/share/document_spec.rb
+++ spec/lib/share/document_spec.rb
@@ -1,5 +1,8 @@
 require "spec_helper"
 
+# unit-ish tests of the Document class. See also integration/document_spec.rb
+# for tests that work documents with collaborators
+
 describe Share::Document do
   describe "initialisation" do
     let!(:doc) { Share::Document.new("foo") }
@@ -26,95 +29,4 @@ describe Share::Document do
 
   end
 
-  describe "#apply_op" do
-    let!(:doc) { Share::Document.new("foo") }
-
-    context "with two in-order insert ops" do
-      before do
-        doc.apply_op(0, {'i' =>'foo', 'p' => 0})
-        doc.apply_op(1, {'i' =>' bar', 'p' => 3})
-      end
-
-      it "should increment the version" do
-        doc.version.should == 2
-      end
-
-      it "should update the value" do
-        doc.value.should == "foo bar"
-      end
-    end
-
-    context "with an in-order insert and delete op" do
-      before do
-        doc.apply_op(0, {'i' =>'foo', 'p' => 0})
-        doc.apply_op(1, {'d' =>'o', 'p' => 2})
-      end
-
-      it "should increment the version" do
-        doc.version.should == 2
-      end
-
-      it "should update the value" do
-        doc.value.should == "fo"
-      end
-    end
-
-    context "with a 2 inserts on the same version that don't conflict" do
-      before do
-        doc.apply_op(0, {'i' =>'bacon', 'p' => 0})
-        doc.apply_op(1, {'i' =>'chunky ', 'p' => 0})
-        doc.apply_op(1, {'i' =>'!', 'p' => 5})
-      end
-
-      it "should increment the version" do
-        doc.version.should == 3
-      end
-
-      it "should update the value" do
-        doc.value.should == "chunky bacon!"
-      end
-    end
-
-    context "with a 2 operations on the same version" do
-      before do
-        doc.apply_op(0, {'i' =>'bacon', 'p' => 0})
-        doc.apply_op(1, {'i' =>'chunky ', 'p' => 0})
-        doc.apply_op(1, {'d' =>'con', 'p' => 2})
-      end
-
-      it "should increment the version" do
-        doc.version.should == 3
-      end
-
-      it "should update the value" do
-        doc.value.should == "chunky ba"
-      end
-    end
-
-    context "with a 2 inserts on the same version that conflict" do
-      before do
-        doc.apply_op(0, {'i' =>'bacon', 'p' => 0})
-        doc.apply_op(1, {'i' =>'chunky ', 'p' => 0})
-        doc.apply_op(1, {'i' =>'cooked ', 'p' => 0})
-      end
-
-      it "should increment the version" do
-        doc.version.should == 3
-      end
-
-      it "should update the value" do
-        doc.value.should == "cooked chunky bacon"
-      end
-    end
-
-    context "with an out-of-order version" do
-
-      it "should raise an exception" do
-        lambda {
-          doc.apply_op(1, {'i' =>' bar', 'p' => 3})
-        }.should raise_error(Share::UnexpectedVersionError)
-      end
-    end
-
-  end
 end
