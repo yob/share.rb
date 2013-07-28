d0f3e7cf15d2dd591bcc7b32e77a4063acf26e0e
James Healy <james@yob.id.au>
2013-07-28 12:17:43 +1000

    move logic for handling an incomming message into Session
    * protocol is not long for this world

--- lib/share/protocol.rb
+++ lib/share/protocol.rb
@@ -9,59 +9,6 @@ module Share
       @current_document = nil
     end
 
-    def respond_to(message)
-      return if message.auth?
-
-      # This got yucky fast.
-      message.document and @current_document = message.document
-
-      response = {doc: @current_document}
-
-      document = @repo.get(@current_document)
-
-      if message.create? && document.exists?
-        response[:create] = false
-      elsif message.create?
-        document = @session.create(@current_document, message.type, {})
-        response[:create] = true
-        response[:meta] = document.meta
-      elsif !document.exists?
-        response[:error] = "Document does not exist"
-      end
-
-      if message.operation?
-        @session.submit_op(@current_document, message.data)
-        return {v: message.data[:v]}
-      end
-
-      if document.type && message.type && document.type != message.type
-        response[:error] = "Type mismatch"
-      end
-
-      if message.open? and response[:error]
-        response[:open] = false
-        return response
-      end
-
-
-      if message.open?
-        @app.subscribe_to(@current_document, message.version)
-        response[:open] = true
-        response[:v] = document.version
-      end
-
-      if message.snapshot?
-        response[:snapshot] = document.snapshot
-      end
-
-      if message.close?
-        @app.unsubscribe_from(@current_document)
-        response = {doc: @current_document, open: false}
-      end
-
-      response
-    end
-
     def message_for_operation(operation)
       {
         doc: @current_document,
